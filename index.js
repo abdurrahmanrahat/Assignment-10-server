7e121170c0d5eaf89e0c3c38ef819dff577f28ae
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-05-12 09:59:47 +0600

    Basic set up in server & install requiments

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Chef is running!!');
+})
+
+app.listen(port, () => {
+    console.log(`Chef API is running on Port: ${port}`);
+})
