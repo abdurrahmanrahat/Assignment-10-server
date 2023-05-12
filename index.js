a898b6da00f41e304abc1d0d13b01c728106c582
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-05-12 10:12:17 +0600

    load data in specific path url

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-
+const chefsData = require('./data/chefs.json');
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Chef is running!!');
+})
+
+// all chef data send to client side.
+app.get('/chefs', (req, res) => {
+    res.send(chefsData);
 })
 
 app.listen(port, () => {
